2f0074fd4ec5820e5a971bb71bf46b15271ef11c
liwenxuan <liwenxuan@sinosoft.com.cn>
2019-09-06 21:07:10 +0800

    cmp-gateway

--- src/main/java/io/cmp/modules/gateway/entity/MessageInfo.java
+++ src/main/java/io/cmp/modules/gateway/entity/MessageInfo.java
@@ -9,7 +9,9 @@ public class MessageInfo {
     private String sourceClientId;
     //目标客户端id
     private String targetClientId;
-    //消息类型
+    //消息方向 1：客户坐席 2：坐席到客户
+    private String direction;
+    //消息类型 chart
     private String msgType;
     //消息内容
     private String msgContent;
