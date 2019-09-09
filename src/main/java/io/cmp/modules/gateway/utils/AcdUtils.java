ece496bf3a32f45ad521722aaae1cb4f5cc226da
liwenxuan <liwenxuan@sinosoft.com.cn>
2019-09-09 23:12:42 +0800

    cmp-gateway

--- src/main/java/io/cmp/modules/gateway/utils/AcdUtils.java
+++ src/main/java/io/cmp/modules/gateway/utils/AcdUtils.java
@@ -1,13 +1,18 @@
 package io.cmp.modules.gateway.utils;
 
+import io.cmp.modules.gateway.entity.AgentInfo;
+
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class AcdUtils {
     public static String distribution(ConcurrentMap agentStatusMap)
     {
-        String agentId="";
-
-
+        String[] keys = (String[])agentStatusMap.keySet().toArray(new String[0]);
+        Random random = new Random();
+        String randomKey = keys[random.nextInt(keys.length)];
+        String agentId = (String)agentStatusMap.get(randomKey);
         return agentId;
     }
 }
