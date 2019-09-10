a7707d9bd8f3ea26b6b6a491ea031163a2839192
liwenxuan <liwenxuan@sinosoft.com.cn>
2019-09-10 10:38:35 +0800

    cmp-gateway

--- src/main/java/io/cmp/modules/gateway/utils/AcdUtils.java
+++ src/main/java/io/cmp/modules/gateway/utils/AcdUtils.java
@@ -10,14 +10,28 @@ import java.util.concurrent.ConcurrentMap;
 
 public class AcdUtils {
     private Logger logger = LoggerFactory.getLogger(getClass());
+
     public String distribution(ConcurrentMap<String, AgentInfo> agentStatusMap)
     {
         String[] keys = (String[])agentStatusMap.keySet().toArray(new String[0]);
-        Random random = new Random();
-        String randomKey = keys[random.nextInt(keys.length)];
-        AgentInfo agentInfo = agentStatusMap.get(randomKey);
-        logger.info("agentInfo="+agentInfo);
-        String agentId = agentInfo.getAgentId();
+
+        String agentId=null;
+        while(true) {
+            Random random = new Random();
+            String randomKey = keys[random.nextInt(keys.length)];
+            logger.info("randomKey=" + randomKey);
+            AgentInfo agentInfo = agentStatusMap.get(randomKey);
+            if("1".equals(agentInfo.getAgentStaus()) || "2".equals(agentInfo.getAgentStaus())) {
+                agentId = agentInfo.getAgentId();
+                logger.info("agentId=" + agentId);
+                break;
+            }
+            else
+            {
+                logger.info("没有空闲坐席");
+            }
+        }
+
         return agentId;
     }
 }
