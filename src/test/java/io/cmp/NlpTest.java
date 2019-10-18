c33f8a7e3566fc1a10ce6e42a2f785b9d8dabe08
liwenxuan <liwenxuan@sinosoft.com.cn>
2019-10-18 14:16:33 +0800

    cmp-gateway nlp测试类提交

---
+++ src/test/java/io/cmp/NlpTest.java
@@ -0,0 +1,25 @@
+package io.cmp;
+
+import com.alibaba.fastjson.JSON;
+import io.cmp.modules.gateway.entity.Nlp;
+import io.cmp.modules.gateway.utils.NlpUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NlpTest {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Test
+    public void nlpSaaSQA() {
+        NlpUtils nlpUtils = new NlpUtils();
+        Nlp nlp=nlpUtils.nlpSaaSQA("78fa31aaf74e4996893ba3fde1ae03a5","你好","YY","盐田区政府","TelecomRobot_YT");
+        logger.info(JSON.toJSONString(nlp));
+    }
+}
